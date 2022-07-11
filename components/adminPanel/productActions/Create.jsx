d64df3cc58d88de0d4f06e92b427407910230d66
StR0vErBG <xcreaw1@abv.bg>
2022-07-12 00:35:47 +0200

    testing

--- components/adminPanel/productActions/Create.jsx
+++ components/adminPanel/productActions/Create.jsx
@@ -4,49 +4,76 @@ import Input from "../../form/AccInput";
 import OutlinedBtn from "../../buttons/Outlined";
 import Article from "./create/Article";
 
-export default function Create() {
-  const articlesObj = {
-    length: 0,
-    items: {
-      length: 0,
-    },
-  };
-  const [articleState, setArticleState] = useState(articlesObj);
+import { create } from "../fetchActions";
 
+export default function Create() {
   const [articles, setArticles] = useState([]);
+
+  const articleStateObj = [];
+  const [articlesState, setArticlesState] = useState(articleStateObj);
+  const [sectionState, setSectionState] = useState({
+    length: 0,
+    articles: articleStateObj,
+  });
 
   const addArticle = (e) => {
     setArticles((lastItems) => [
       ...lastItems,
-      <Article key={articles.length} articleState={articleState} />,
+      <Article
+        key={sectionState.length}
+        articleLength={sectionState.length}
+        articlesState={articlesState}
+        setArticlesState={setArticlesState}
+      />,
     ]);
-    setArticleState((prevState) => ({
+    setSectionState((prevState) => ({
       ...prevState,
-      length: articlesObj.length + 1,
+      length: sectionState.length + 1,
     }));
   };
 
-  const createHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const userData = Object.fromEntries(formData);
-    console.log(userData);
+    console.log(articlesState, sectionState);
+    // const res = await create(articleState);
+    // const data = await res.json();
+    // console.log(res, data);
   };
+  const changeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setSectionState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <div>
         <button>Nqkude</button>
       </div>
       <div>
-        <form onSubmit={createHandler}>
-          <Input type="text" placeholder="Секция" id="section" />
-          <Input type="file" placeholder="Снимка" id="imageUrl" />
+        <form onSubmit={submitHandler}>
+          <Input
+            type="text"
+            placeholder="Секция"
+            id="sectionName"
+            value={sectionState.section}
+            onChange={changeHandler}
+          />
+          <Input
+            type="file"
+            placeholder="Снимка"
+            id="imageUrl"
+            onChange={changeHandler}
+          />
           {articles}
           <div className="">
             <button
               type="button"
               onClick={() => addArticle()}
-              className="flex  justify-center mx-auto bg-orange  py-2 px-14 text-white rounded-md font-medium "
+              className="flex justify-center py-2 mx-auto font-medium text-white rounded-md bg-orange px-14 "
             >
               Добави артикул
             </button>
