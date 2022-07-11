4e480ed0c2dbfb34a216f27e2a7e145b040d0d11
StR0vErBG <xcreaw1@abv.bg>
2022-07-11 11:43:45 +0200

    adding styling

--- components/adminPanel/Navigation.jsx
+++ components/adminPanel/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -6,22 +6,31 @@ import navDictionary from "./navDictionary";
 export default function Navigation() {
   const router = useRouter();
 
+  const [isCategory, setIsCategory] = useState(null);
+
+  useEffect(() => {
+    const hash = window.location.hash.slice(1);
+
+    setIsCategory(hash);
+  }, [router]);
   const changeCategory = (category, e) => {
     console.log(e);
     // Trigger fragment change to fetch the new data
     router.push(`/adminPanel/#${category}`, undefined, { shallow: true });
   };
   return (
-    <aside className="container">
-      <ul className="flex gap-2 mt-5">
-        <li className="cursor-pointer">
+    <aside className="container bg-primary shadow-lg  sm:rounded-bl-full sm:rounded-br-full text-center">
+      <ul className="sm:flex gap-2  items-center justify-center grid-col-1 xs:grid-cols-2 grid text-white">
+        <li className="cursor-pointer hover:bg-primary-0 hover:text-green p-2 font-medium">
           <Link href="/">Начало</Link>
         </li>
 
         {navDictionary.map((item) => {
           return (
             <li
-              className="cursor-pointer"
+              className={`cursor-pointer hover:bg-primary-0 p-2 font-medium hover:text-green
+              ${item.route == isCategory && "text-green"}
+              `}
               key={item.text}
               onClick={(e) => changeCategory(item.route, e)}
             >
