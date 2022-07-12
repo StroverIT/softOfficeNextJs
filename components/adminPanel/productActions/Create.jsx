ecacc7f8f374ef189f89208fc8450915c9785388
StR0vErBG <xcreaw1@abv.bg>
2022-07-12 12:06:13 +0200

    fully styled and working create

--- components/adminPanel/productActions/Create.jsx
+++ components/adminPanel/productActions/Create.jsx
@@ -1,43 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, userId } from "react";
 
+import { v4 as uuidv4 } from "uuid";
+// Components
 import Input from "../../form/AccInput";
 import OutlinedBtn from "../../buttons/Outlined";
 import Article from "./create/Article";
 
 import { create } from "../fetchActions";
+import { ProductContext } from "./productContext";
 
 export default function Create() {
   const [articles, setArticles] = useState([]);
 
-  const articleStateObj = [];
-  const [articlesState, setArticlesState] = useState(articleStateObj);
   const [sectionState, setSectionState] = useState({
-    length: 0,
-    articles: articleStateObj,
+    secLen: 0,
+    articles: [],
   });
 
   const addArticle = (e) => {
+    const keyProduct = uuidv4();
+    const initVal = {};
     setArticles((lastItems) => [
       ...lastItems,
-      <Article
-        key={sectionState.length}
-        articleLength={sectionState.length}
-        articlesState={articlesState}
-        setArticlesState={setArticlesState}
-      />,
+      <Article key={keyProduct} articleLen={sectionState.secLen} />,
     ]);
     setSectionState((prevState) => ({
       ...prevState,
-      length: sectionState.length + 1,
+      secLen: sectionState.secLen + 1,
+      articles: [...prevState.articles, initVal],
     }));
   };
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(articlesState, sectionState);
-    // const res = await create(articleState);
-    // const data = await res.json();
-    // console.log(res, data);
+    const res = await create(articleState);
+    const data = await res.json();
+    console.log(res, data);
   };
   const changeHandler = (e) => {
     const name = e.target.name;
@@ -51,39 +49,43 @@ export default function Create() {
   return (
     <div>
       <div>
-        <button>Nqkude</button>
+        <button>
+          Nqkude {sectionState.secLen} {sectionState.articles?.length}
+        </button>
       </div>
       <div>
-        <form onSubmit={submitHandler}>
-          <Input
-            type="text"
-            placeholder="Секция"
-            id="sectionName"
-            value={sectionState.section}
-            onChange={changeHandler}
-          />
-          <Input
-            type="file"
-            placeholder="Снимка"
-            id="imageUrl"
-            onChange={changeHandler}
-          />
-          {articles}
-          <div className="">
-            <button
-              type="button"
-              onClick={() => addArticle()}
-              className="flex justify-center py-2 mx-auto font-medium text-white rounded-md bg-orange px-14 "
-            >
-              Добави артикул
-            </button>
-          </div>
-          <OutlinedBtn
-            type="submit"
-            text="Създай"
-            custom="bg-green border-green hover:text-green rounded-md md:w-1/4 my-5 transition-colors"
-          />
-        </form>
+        <ProductContext.Provider value={{ sectionState, setSectionState }}>
+          <form onSubmit={submitHandler}>
+            <Input
+              type="text"
+              placeholder="Секция"
+              id="sectionName"
+              value={sectionState.section}
+              onChange={changeHandler}
+            />
+            <Input
+              type="file"
+              placeholder="Снимка"
+              id="imageUrl"
+              onChange={changeHandler}
+            />
+            {articles}
+            <div className="">
+              <button
+                type="button"
+                onClick={() => addArticle()}
+                className="flex justify-center py-2 mx-auto font-medium text-white rounded-md bg-orange px-14 "
+              >
+                Добави артикул
+              </button>
+            </div>
+            <OutlinedBtn
+              type="submit"
+              text="Създай"
+              custom="bg-green border-green hover:text-green rounded-md md:w-1/4 my-5 transition-colors"
+            />
+          </form>
+        </ProductContext.Provider>
       </div>
     </div>
   );
