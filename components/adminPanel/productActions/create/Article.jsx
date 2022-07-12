f83239944997a936c0156937a34ae3db39d5b41d
StR0vErBG <xcreaw1@abv.bg>
2022-07-12 12:06:40 +0200

    fully styled and working article

--- components/adminPanel/productActions/create/Article.jsx
+++ components/adminPanel/productActions/create/Article.jsx
@@ -1,39 +1,63 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import Input from "../../../form/AccInput";
+import { ProductContext } from "../productContext";
+import { v4 as uuidv4 } from "uuid";
 
 import Item from "./Item";
 
-function Article({ articlesState, setArticlesState, articleLength }) {
-  const [currArticleState, setCurrArticleState] = useState({});
+function Article({ articleLen }) {
+  const { sectionState, setSectionState } = useContext(ProductContext);
+
+  const [articlesState, setArticleState] = useState({});
   const [items, setItems] = useState([]);
 
   const addItem = (e) => {
+    const itemId = uuidv4();
+    let itemLen;
+
+    const articles = sectionState.articles.map((article, index) => {
+      if (index == articleLen) {
+        if (!article.items) {
+          article.items = [];
+          article.itemsLen = 0;
+        }
+        article.items.push({});
+        article.itemsLen = article.itemsLen + 1;
+        itemLen = article.itemsLen;
+      }
+      return article;
+    });
+
+    setSectionState((prevState) => ({
+      ...prevState,
+      articles: [...articles],
+    }));
+
     setItems((lastItems) => [
       ...lastItems,
-      <Item
-        key={itemId}
-        articleState={articleState}
-        setArticleState={setArticleState}
-      />,
+      <Item key={itemId} articleLen={articleLen} itemLen={itemLen} />,
     ]);
   };
   const changeHandler = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setCurrArticleState((prevState) => ({
+    setArticleState((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
   useEffect(() => {
-    const updatedArticle = articlesState.map((item) => {
-      if (item.length == articleLength) {
-        return { ...item, ...currArticleState };
+    const articles = sectionState.articles.map((item, index) => {
+      if (index == articleLen) {
+        return { ...item, ...articlesState };
       }
       return item;
     });
-    setArticlesState(updatedArticle);
+    setSectionState((prevState) => ({
+      ...prevState,
+      articles: [...articles],
+    }));
   }, [articlesState]);
   return (
     <div className="px-2 py-5 border rounded-sm border-gray my-9">
