356604a9dfdd9e4a6342b345f0266e41450b731a
StR0vErBG <xcreaw1@abv.bg>
2022-07-12 12:18:12 +0200

    fixing btn type submit to btn

--- components/adminPanel/productActions/create/Item.jsx
+++ components/adminPanel/productActions/create/Item.jsx
@@ -9,6 +9,7 @@ function IsComponent({ state, setState, onText, offText }) {
       className={`px-5 py-1 transition-transform border rounded-full text-semibold ${
         !state ? "border-green text-green" : "border-secondary text-secondary"
       } hover:-translate-y-1`}
+      type="button"
     >
       {!state ? onText : offText}
     </button>
