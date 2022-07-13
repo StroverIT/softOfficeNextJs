a6b06861e6e4b29276b9838e4619b0f467988367
StR0vErBG <xcreaw1@abv.bg>
2022-07-13 18:32:30 +0200

    Removing article

--- components/adminPanel/productActions/create/Article.jsx
+++ components/adminPanel/productActions/create/Article.jsx
@@ -5,19 +5,49 @@ import { ProductContext } from "../productContext";
 import { v4 as uuidv4 } from "uuid";
 
 import Item from "./Item";
+import { HiX } from "react-icons/hi";
+
+const len = {};
 
 function Article({ articleLen }) {
-  const { sectionState, setSectionState } = useContext(ProductContext);
+  const { sectionState, setSectionState, articlesComp, setArticlesComp } =
+    useContext(ProductContext);
 
+  const [articleLenState, setArticleLenState] = useState(len);
   const [articlesState, setArticleState] = useState({});
   const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    len.length = articleLen;
+    console.log(articleLenState);
+  }, []);
+  const removeArticle = (e) => {
+    const filtered = articlesComp.filter((article) => {
+      return article.props.articleLen != articleLenState.length;
+    });
+    let filteredObj;
+    if (articlesComp.length > 1) {
+      filteredObj = sectionState.articles.filter(function (x, index) {
+        if (index != articleLenState.length) {
+          return x;
+        }
+      });
+    } else filteredObj = [];
+
+    setSectionState((prevState) => ({
+      ...prevState,
+      secLen: sectionState.secLen - 1,
+      articles: filteredObj,
+    }));
+    setArticlesComp(filtered);
+    setArticleLenState({ length: len.length - 1 });
+  };
   const addItem = (e) => {
     const itemId = uuidv4();
     let itemLen;
 
     const articles = sectionState.articles.map((article, index) => {
-      if (index == articleLen) {
+      if (index == articleLenState.length) {
         if (!article.items) {
           article.items = [];
           article.itemsLen = 0;
@@ -36,7 +66,11 @@ function Article({ articleLen }) {
 
     setItems((lastItems) => [
       ...lastItems,
-      <Item key={itemId} articleLen={articleLen} itemLen={itemLen} />,
+      <Item
+        key={itemId}
+        articleLen={articleLenState.length}
+        itemLen={itemLen}
+      />,
     ]);
   };
   const changeHandler = (e) => {
@@ -46,10 +80,11 @@ function Article({ articleLen }) {
       ...prevState,
       [name]: value,
     }));
+    console.log(articleLenState);
   };
   useEffect(() => {
     const articles = sectionState.articles.map((item, index) => {
-      if (index == articleLen) {
+      if (index == articleLenState.length) {
         return { ...item, ...articlesState };
       }
       return item;
@@ -62,6 +97,16 @@ function Article({ articleLen }) {
   }, [articlesState]);
   return (
     <div className="px-2 py-5 border rounded-sm border-gray my-9">
+      <div className="flex justify-end">
+        <button
+          type="button"
+          className="text-secondary text-lg"
+          onClick={removeArticle}
+        >
+          <HiX />
+        </button>
+      </div>
+
       <Input
         type="text"
         placeholder="Име на артикула"
