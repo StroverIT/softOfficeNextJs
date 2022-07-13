7c3468aeb5f9f909719dd27ea2d258c933991dbc
StR0vErBG <xcreaw1@abv.bg>
2022-07-13 17:22:42 +0200

    small dictionary, removing headers and fixing body

--- components/adminPanel/fetchActions.js
+++ components/adminPanel/fetchActions.js
@@ -1,9 +1,12 @@
 const route = "/api/products";
 
+const multipart = "multipart/data;boundary";
+const formData = "multipart/form-data;";
+const appJson = "application/json";
+
 export const create = async (data) => {
   return fetch(`${route}/create`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
+    body: data,
   });
 };
