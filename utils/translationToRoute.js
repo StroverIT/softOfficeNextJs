a7394e843a934731bc9ff5a12618a2de5d5c8f1b
StR0vErBG <xcreaw1@abv.bg>
2022-07-16 23:53:12 +0200

    removing unnesceserry characters

--- utils/translationToRoute.js
+++ utils/translationToRoute.js
@@ -1,10 +1,15 @@
 import { cyrylicToLatin } from "./translations";
-
+/*
+ Todo:
+ Remove the commas or semi colons
+*/
 const translationToRoute = (data) => {
   return data
     .toString()
     .split(" ")
     .map((word) => {
+      word = word.replace(",", "");
+
       return cyrylicToLatin(word);
     })
     .join("-");
