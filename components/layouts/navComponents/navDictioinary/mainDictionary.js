57906a57b230e6673a67eb93e71225473df76682
StR0vErBG <xcreaw1@abv.bg>
2022-07-16 17:41:55 +0200

    removing route and adding to be dynamic

--- components/layouts/navComponents/navDictioinary/mainDictionary.js
+++ components/layouts/navComponents/navDictioinary/mainDictionary.js
@@ -18,38 +18,32 @@ articles: {
 const dictionary = [
   {
     title: "Офис материали",
-    mainRoute: "ofis-materiali",
     // Menu
     articles: officeMatArt,
     icon: "",
   },
   {
     title: "Техника и консумативи",
-    mainRoute: "tehnika-i-konsymativi",
     articles: tehnikiKonsymativiArt,
     icon: "",
   },
   {
     title: "Средства за презентация",
-    mainRoute: "sredstva-za-prezentaciq",
     articles: sredstvaZaPrezentaciqArt,
     icon: "",
   },
   {
     title: "Храни и напитки",
-    mainRoute: "hrani-i-napitki",
     articles: hraniINapitkiArt,
     icon: "",
   },
   {
     title: "Хигиенни средства",
-    mainRoute: "heatingCoolingAndVick",
     articles: higienniSredstvaArt,
     icon: "",
   },
   {
     title: "Обзавеждане за офис",
-    mainRoute: "floorAndWallCoverings",
     articles: obzavejdaneZaOfisArt,
     icon: "",
   },
