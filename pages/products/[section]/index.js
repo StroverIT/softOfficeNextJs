f983cb2081d9ee7705300309a03b872f9fc03092
StR0vErBG <xcreaw1@abv.bg>
2022-07-16 19:21:35 +0200

    exporting the dictionary

--- pages/products/[section]/index.js
+++ pages/products/[section]/index.js
@@ -18,42 +18,8 @@ import Product from "./../../../components/products/listProducts/Product";
 import Sorting from "./../../../components/products/filters/Sorting";
 import RangeSlider from "./../../../components/products/aside/RangeSlider";
 // Dictionaries
-const sortByDictionary = [
-  {
-    val: "nameAsc",
-    text: "Име (възходящ ред)",
-  },
-  {
-    val: "nameDesc",
-    text: "Име (низходящ ред)",
-  },
-  {
-    val: "priceAsc",
-    text: "Цена (възходящ ред)",
-  },
-  {
-    val: "priceDesc",
-    text: "Цена (низходящ ред)",
-  },
-];
-const pageDictionary = [
-  {
-    val: "6",
-    text: "по 6 на страница",
-  },
-  {
-    val: "12",
-    text: "по 12 на страница",
-  },
-  {
-    val: "24",
-    text: "по 24 на страница",
-  },
-  {
-    val: "48",
-    text: "по 48 на страница",
-  },
-];
+import { sortByDictionary, pageDictionary } from "./allProductDicFilters";
+
 export default function Section() {
   const router = useRouter();
   const { section } = router.query;
