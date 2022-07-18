461c2f9f24aef2e17a0df0322edfbf3df780c8be
StR0vErBG <xcreaw1@abv.bg>
2022-07-18 02:54:45 +0200

    almost fully working product

--- components/products/listProducts/Product.jsx
+++ components/products/listProducts/Product.jsx
@@ -6,13 +6,16 @@ import Link from "next/link";
 //Components
 import Pricing from "../../priceStyling/Pricing";
 import BuyBtn from "../../base/BuyBtn";
+// Translator
 
-export default function Product({ section }) {
+export default function Product({ article, item, commonName, sectionName }) {
+  const price = item.price.toFixed(2).split(".");
+  const itemTypes = item.types.toString().split("\n");
   return (
     <section className="flex items-center mb-5 border border-gray">
       <div className="md:grid grid-cols-[80%20%] w-full h-full">
         <div className="items-center h-full py-3 sm:flex">
-          <Link href={`/products/${section}/someProduct`}>
+          <Link href={`/products/${sectionName}/prodykta`}>
             <div className="relative flex w-full h-48 cursor-pointer sm:w-52 sm:h-32">
               <Image
                 src="/images/testCarousel.jpg"
@@ -23,24 +26,22 @@ export default function Product({ section }) {
           </Link>
 
           <div className="pl-4">
-            <Link href={`/products/${section}/someProduct`}>
+            <Link href={`/products/${sectionName}/prodykta`}>
               <h2 className="text-lg cursor-pointer">
-                АКУМУЛАТОРЕН КОМПЛЕКТ EINHELL TE-TK 12 Li
+                {commonName} {article.articleName}
               </h2>
             </Link>
             <ul className="pt-2 text-sm text-gray-darker">
-              <li>ИНСТРУМЕНТ 1 : АКУМУЛАТОРНА БОРМАШИНА</li>
-              <li>ИНСТРУМЕНТ 2 : МУЛТИФУНКЦИОНАЛНО УСТРОЙСТВО</li>
-              <li>ИНСТРУМЕНТ 3 : АКУМУЛАТОРЕН ФЕНЕР</li>
-              <li>ТИП БАТЕРИЯ : Li-Ion</li>
-              <li>НАПРЕЖЕНИЕ : 12.00 V</li>
+              {itemTypes.map((type) => {
+                return <li key={type}>{type}</li>;
+              })}
             </ul>
           </div>
         </div>
         <div className="bg-grayBg">
           <div className="container flex flex-col justify-center h-full itesm-center">
             <div className="flex justify-center">
-              <Pricing price={169} priceDec={99} size="2xl" />
+              <Pricing price={price[0]} priceDec={price[1]} size="2xl" />
             </div>
             {/* Create grayer color for future*/}
             <BuyBtn border={true} />
