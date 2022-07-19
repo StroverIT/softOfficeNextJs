00914c83180d88ca1f958a4206d0f3bf8d173304
StR0vErBG <xcreaw1@abv.bg>
2022-07-19 11:52:10 +0200

    getting product by itemId

--- services/productService.js
+++ services/productService.js
@@ -1,12 +1,38 @@
 import { connectMongo } from "../db/connectDb";
+import mongoose from "mongoose";
 
 import Product from "../db/models/Product";
+
 export const getAllProducts = async (sectionName, filter) => {
   await connectMongo();
   // for case insensitive
+
   return Product.findOne({
     sectionName: {
       $regex: new RegExp(sectionName, "i"),
     },
   });
+};
+export const productByItemId = async (itemId) => {
+  await connectMongo();
+
+  let obj = await Product.findOne({ "articles.items._id": itemId });
+
+  let formatedObj = {};
+
+  formatedObj.sectionName = obj.sectionName;
+  formatedObj.commonName = obj.commonName;
+  let imageUrl = "";
+
+  outer: for (let article of obj.articles) {
+    formatedObj.description = article.description;
+    formatedObj.articleName = article.articleName;
+    for (let item of article.items) {
+      if (item._id == itemId) {
+        formatedObj.item = item;
+        break outer;
+      }
+    }
+  }
+  return formatedObj;
 };
