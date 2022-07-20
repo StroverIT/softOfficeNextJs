e0368a60fba793ac19e906ee21cf55a3799fb630
StR0vErBG <xcreaw1@abv.bg>
2022-07-20 10:26:20 +0200

    adding on click

--- components/base/BuyBtn.jsx
+++ components/base/BuyBtn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const sizing = {
   lg: " px-5 md:px-16 py-5 md:text-xl",
 };
-export default function BuyBtn({ border, lg }) {
+export default function BuyBtn({ border, lg, onClick }) {
   return (
     <div className={`${border ? "border-t border-gray" : ""}`}>
       <button
@@ -11,6 +11,7 @@ export default function BuyBtn({ border, lg }) {
         className={`${
           lg ? sizing.lg : ""
         } w-full py-1 my-10 font-semibold text-white rounded-full bg-primary lg:my-5`}
+        onClick={onClick}
       >
         Купи
       </button>
