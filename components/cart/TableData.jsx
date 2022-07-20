1ccfeba92eb5d16ace31fcc4b31885a7f2a3b1fa
StR0vErBG <xcreaw1@abv.bg>
2022-07-20 10:26:07 +0200

    chaning to separated component

---
+++ components/cart/TableData.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function TableData({ children, classes }) {
+  return (
+    <td className={`w-full lg:w-auto ${classes ? classes : ""}`}>{children}</td>
+  );
+}
+export default TableData;
