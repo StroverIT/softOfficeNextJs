39886984ef4e0ad4bc49c71f9e8e5173e24fee01
StR0vErBG <xcreaw1@abv.bg>
2022-07-21 21:11:50 +0200

    adding route to the cart item

--- components/cart/CartItem.jsx
+++ components/cart/CartItem.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+
+// Components
 import ImageAndListTableData from "./ImageAndListTableData";
 import LgScreenTableData from "./LgScreenTableData";
 import MobileScreenTableData from "./MobileScreenTableData";
+// Translator
+import { translationToRoute } from "../../utils/translationToRoute";
 
 export default function CartItem({ cartData, removeProduct, changeQty }) {
+  const route = `/products/${translationToRoute(cartData.item.sectionName)}/${
+    cartData.item._id
+  }`;
   return (
     <tr className="border-b border-gray-[#e4e7e6] flex flex-wrap lg:table-row justify-between items-center pb-3 mb-3">
       {/* Image plus list */}
       <ImageAndListTableData
         listData={cartData?.item.types[0].split("\n")}
         cartName={cartData?.item.articleName}
+        route={route}
       />
       {/* MObile version */}
       <MobileScreenTableData
--- components/cart/ImageAndListTableData.jsx
+++ components/cart/ImageAndListTableData.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import Image from "next/image";
-
+import Link from "next/link";
 import TableData from "./TableData";
 
-export default function ImageAndListTableData({ image, listData, cartName }) {
+export default function ImageAndListTableData({
+  image,
+  listData,
+  cartName,
+  route,
+}) {
   return (
     <>
       <TableData>
         <div className="flex items-center justify-center lg:justify-start">
-          <div className="relative w-full h-40 sm:w-2/3 sm:h-60 lg:h-32 lg:w-40 ">
-            <Image
-              layout="fill"
-              src="/images/testCarousel.jpg"
-              alt="just for testing"
-            />
-          </div>
+          <Link href={route}>
+            <div className="relative w-full h-40 sm:w-2/3 sm:h-60 lg:h-32 lg:w-40 cursor-pointer ">
+              <Image
+                layout="fill"
+                src="/images/testCarousel.jpg"
+                alt="just for testing"
+              />
+            </div>
+          </Link>
         </div>
       </TableData>
       <TableData classes="mt-3 sm:text-center lg:text-left xl:pl-2">
-        <h3 className="">{cartName}</h3>
+        <Link href={route}>
+          <h3 className="cursor-pointer">{cartName}</h3>
+        </Link>
         <ul className="mt-2 text-xs text-gray-250">
           {listData.slice(0, 5).map((list) => {
             return (
