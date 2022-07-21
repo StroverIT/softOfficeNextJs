d41f132c701b1f1e6891a8c27c7c4f5ef02c4dac
StR0vErBG <xcreaw1@abv.bg>
2022-07-21 12:33:15 +0200

    this was not the bug, but is still good to have

--- components/cart/CartItem.jsx
+++ components/cart/CartItem.jsx
@@ -8,20 +8,20 @@ export default function CartItem({ cartData, removeProduct, changeQty }) {
     <tr className="border-b border-gray-[#e4e7e6] flex flex-wrap lg:table-row justify-between items-center pb-3 mb-3">
       {/* Image plus list */}
       <ImageAndListTableData
-        listData={cartData.item.types[0].split("\n")}
-        cartName={cartData.item.articleName}
+        listData={cartData?.item.types[0].split("\n")}
+        cartName={cartData?.item.articleName}
       />
       {/* MObile version */}
       <MobileScreenTableData
-        price={cartData.item.price}
-        qty={cartData.qty}
+        price={cartData?.item.price}
+        qty={cartData?.qty}
         removeProduct={removeProduct}
         changeQty={changeQty}
       />
       {/* Large screen version */}
       <LgScreenTableData
-        price={cartData.item.price}
-        qty={cartData.qty}
+        price={cartData?.item.price}
+        qty={cartData?.qty}
         removeProduct={removeProduct}
         changeQty={changeQty}
       />
