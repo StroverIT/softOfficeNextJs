78b44fb4c257df76a578fd25f1068e9b10829a0b
StR0vErBG <xcreaw1@abv.bg>
2022-07-22 13:53:04 +0200

    adding name

--- components/buttons/Outlined.jsx
+++ components/buttons/Outlined.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Outlined({ type, text, custom, isDisabled }) {
+export default function Outlined({ type, text, custom, isDisabled, name }) {
   return (
     <button
       type={type}
       disabled={isDisabled}
+      name={name}
       className={`w-full py-2 font-medium text-white uppercase transition-colors duration-100 border px-14 bg-primary hover:bg-transparent hover:text-primary border-primary ${
         custom ? custom : ""
       }`}
