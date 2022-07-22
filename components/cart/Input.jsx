904bb5feb789d205b66daf59662d2afa59ef6615
StR0vErBG <xcreaw1@abv.bg>
2022-07-22 13:52:54 +0200

    adding value and onChange handler

--- components/cart/Input.jsx
+++ components/cart/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Input({ id, text, type, holder }) {
+function Input({ id, text, type, holder, value, handler }) {
   return (
     <div className="flex flex-col justify-between mb-2 lg:mb-0 lg:flex-row">
       <div className="flex items-center ">
@@ -13,7 +13,9 @@ function Input({ id, text, type, holder }) {
         id={id}
         name={id}
         className="px-3 py-2 my-1 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline placeholder:text-gray-200"
+        value={value}
         placeholder={holder}
+        onChange={handler}
       />
     </div>
   );
