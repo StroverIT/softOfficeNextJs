fdb4e5c3aa69198bc623a41799e26587813d0e3a
StR0vErBG <xcreaw1@abv.bg>
2022-07-22 13:02:23 +0200

    adding if is loggged in

--- pages/cart.js
+++ pages/cart.js
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 // NextJs
 import Head from "next/head";
+import { getSession } from "next-auth/react";
 
 // Components
 import Price from "../components/priceStyling/Pricing";
@@ -11,10 +12,18 @@ import CartItem from "../components/cart/CartItem";
 //Redux
 import { connect } from "react-redux";
 import { adjustQty, removeFromCart } from "../redux/actions/productActions";
-function Cart({ cart, adjustQty, removeFromCart }) {
+// Utils
+import isObjectEmpty from "../utils/isObjectEmpty";
+import Link from "next/link";
+
+function Cart({ cart, adjustQty, removeFromCart, userData }) {
+  console.log(userData);
   const addInfo = useRef(null);
+
   const [isHidd, setHidd] = useState(true);
   const [hidText, setHidText] = useState("Добави адрес");
+  const [isDelivery, setIsDelivery] = useState(false);
+
   let subtotal = cart
     .map((item) => {
       return item.item.price * item.qty;
@@ -80,7 +89,7 @@ function Cart({ cart, adjustQty, removeFromCart }) {
                 </tbody>
               </table>
 
-              <aside className="border-[10px] border-gray-100 p-5">
+              <aside className="border-[10px] border-gray-100 p-5 relative">
                 <section className="flex items-center justify-between border-b b-[#e4e7e6] py-2">
                   <div className="font-semibold uppercase ">Междинна сума:</div>
                   <div>
@@ -102,91 +111,116 @@ function Cart({ cart, adjustQty, removeFromCart }) {
                       {hidText}
                     </button>
                   </section>
-                  <section className={isHidd ? "hidden" : ""} ref={addInfo}>
-                    <form action="">
-                      <section className="container w-full mt-3">
-                        <Input
-                          id="name"
-                          type="text"
-                          text="Име"
-                          holder="Иван Иванов"
-                        />
-                        <Input
-                          id="telNumber"
-                          type="number"
-                          text="Телефон"
-                          holder="087 123 4561"
-                        />
-                        <Input
-                          id="city"
-                          type="text"
-                          text="Град"
-                          holder="София"
-                        />
-                        <Input
-                          id="poshtenskiKod"
-                          type="text"
-                          text="Пощенски код"
-                          holder="1584"
-                        />
-                        <div className="flex flex-col justify-between ">
-                          <div className="flex items-center mb-1">
+                  {!isHidd && (
+                    <section ref={addInfo}>
+                      <form action="">
+                        <section className="container w-full mt-3">
+                          <Input
+                            id="name"
+                            type="text"
+                            text="Име"
+                            holder="Иван Иванов"
+                          />
+                          <Input
+                            id="telNumber"
+                            type="number"
+                            text="Телефон"
+                            holder="087 123 4561"
+                          />
+                          <Input
+                            id="city"
+                            type="text"
+                            text="Град"
+                            holder="София"
+                          />
+                          <Input
+                            id="poshtenskiKod"
+                            type="text"
+                            text="Пощенски код"
+                            holder="1584"
+                          />
+                          <div className="flex flex-col justify-between ">
+                            <div className="flex items-center mb-1">
+                              <label
+                                htmlFor="address"
+                                className="font-medium text-dark-400"
+                              >
+                                Адрес
+                              </label>
+                            </div>
+                            <textarea
+                              id="address"
+                              type="text"
+                              text="Адрес"
+                              placeholder="РУМ Дружба 2 срещу блок 205"
+                              className="px-3 py-1 text-sm leading-tight text-gray-700 border rounded shadow appearance-none resize-none focus:outline-none focus:shadow-outline placeholder:text-gray-200"
+                              cols="22"
+                              rows="2"
+                            ></textarea>
+                          </div>
+                        </section>
+
+                        <div className="mt-2 ">
+                          <div className="mb-2">
                             <label
-                              htmlFor="address"
+                              htmlFor="moreInfo"
                               className="font-medium text-dark-400"
                             >
-                              Адрес
+                              Коментар
                             </label>
                           </div>
                           <textarea
-                            id="address"
-                            type="text"
-                            text="Адрес"
-                            placeholder="РУМ Дружба 2 срещу блок 205"
-                            className="px-3 py-1 text-sm leading-tight text-gray-700 border rounded shadow appearance-none resize-none focus:outline-none focus:shadow-outline placeholder:text-gray-200"
-                            cols="22"
+                            name="moreInfo"
+                            id="moreInfo"
+                            cols="10"
                             rows="2"
+                            className="w-full p-3 px-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none resize-none focus:outline-none focus:shadow-outline placeholder:text-gray-200"
                           ></textarea>
                         </div>
-                      </section>
-
-                      <div className="mt-2 ">
-                        <div className="mb-2">
-                          <label
-                            htmlFor="moreInfo"
-                            className="font-medium text-dark-400"
+                        <div className="flex justify-center mt-2">
+                          <button
+                            type="button"
+                            className="w-full py-2 text-sm font-medium text-white uppercase transition-colors duration-300 border px-14 bg-dark hover:bg-transparent hover:text-dark border-dark"
                           >
-                            Коментар
-                          </label>
+                            ОЦЕНКА НА ДОСТАВКАТА
+                          </button>
                         </div>
-                        <textarea
-                          name="moreInfo"
-                          id="moreInfo"
-                          cols="10"
-                          rows="2"
-                          className="w-full p-3 px-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none resize-none focus:outline-none focus:shadow-outline placeholder:text-gray-200"
-                        ></textarea>
-                      </div>
-                      <div className="flex justify-center mt-2">
-                        <button
-                          type="button"
-                          className="w-full py-2 text-sm font-medium text-white uppercase transition-colors duration-300 border px-14 bg-dark hover:bg-transparent hover:text-dark border-dark"
-                        >
-                          ОЦЕНКА НА ДОСТАВКАТА
-                        </button>
-                      </div>
-                    </form>
-                  </section>
+                      </form>
+                    </section>
+                  )}
                 </section>
-                <section className="flex items-center justify-between py-2 mb-2">
-                  <div className="font-semibold uppercase ">Обща цена:</div>
-                  <div>
-                    <Price size="3xl" price={200} priceDec={20} />
+                {isDelivery && (
+                  <>
+                    <section className="flex items-center justify-between py-2 mb-2">
+                      <div className="font-semibold uppercase ">Обща цена:</div>
+                      <div>
+                        <Price size="3xl" price={200} priceDec={20} />
+                      </div>
+                    </section>
+                    <div className="flex justify-center">
+                      <BtnOutlined type="button" text="завърши" />
+                    </div>
+                  </>
+                )}
+                {isObjectEmpty(userData) && (
+                  <div className="absolute flex flex-col items-center justify-center w-full h-full text-white -translate-x-1/2 -translate-y-1/2 opacity-75 top-1/2 left-1/2 bg-dark">
+                    <div className="text-lg text-primary-lighter">
+                      Трябва да имате акаунт
+                    </div>
+                    <div className="flex">
+                      <Link href="/account/register">
+                        <a className="cursor-pointer hover:text-primary-lighter">
+                          Регистрация
+                        </a>
+                      </Link>
+                      <Link href="/account/login">
+                        <a className="ml-1 cursor-pointer hover:text-primary-lighter">
+                          Вход
+                        </a>
+                      </Link>
+                    </div>
                   </div>
-                </section>
-                <div className="flex justify-center">
-                  <BtnOutlined type="button" text="завърши" />
-                </div>
+                )}
               </aside>
             </div>
           )}
@@ -199,6 +233,27 @@ function Cart({ cart, adjustQty, removeFromCart }) {
       </main>
     </>
   );
+}
+export async function getServerSideProps(context) {
+  // Session
+  const session = await getSession({ req: context.req });
+  let data = {};
+  // Mongodb
+  if (session) {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/getUser`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: session.user.email,
+        name: session.user.name,
+      }),
+    });
+    data = await res.json();
+  }
+
+  return {
+    props: { userData: JSON.parse(JSON.stringify(data)) },
+  };
 }
 export default connect(
   (state) => ({
