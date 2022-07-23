bd6be6fb7b5d7bafc1f79c59a46d6e05b5216772
StR0vErBG <xcreaw1@abv.bg>
2022-07-23 22:38:52 +0200

    fixing layout

--- components/cart/ImageAndListTableData.jsx
+++ components/cart/ImageAndListTableData.jsx
@@ -14,7 +14,7 @@ export default function ImageAndListTableData({
       <TableData>
         <div className="flex items-center justify-center lg:justify-start">
           <Link href={route}>
-            <div className="relative w-full h-40 sm:w-2/3 sm:h-60 lg:h-32 lg:w-40 cursor-pointer ">
+            <div className="relative w-full h-40 cursor-pointer sm:w-2/3 sm:h-60 lg:h-32 lg:w-40 ">
               <Image
                 layout="fill"
                 src="/images/testCarousel.jpg"
@@ -24,7 +24,7 @@ export default function ImageAndListTableData({
           </Link>
         </div>
       </TableData>
-      <TableData classes="mt-3 sm:text-center lg:text-left xl:pl-2">
+      <TableData classes="mt-3 text-center lg:text-left xl:pl-2">
         <Link href={route}>
           <h3 className="cursor-pointer">{cartName}</h3>
         </Link>
