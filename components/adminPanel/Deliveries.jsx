e195878d6afa827fa78b4d6454c2dc24ee0eb18c
StR0vErBG <xcreaw1@abv.bg>
2022-07-23 12:03:35 +0200

    creatig delivery componnt

--- components/adminPanel/Deliveries.jsx
+++ components/adminPanel/Deliveries.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Deliveries() {
-  return <div>Deliveries</div>;
+import Section from "./deliveriesComp/Section";
+
+export default function Deliveries({ deliveries }) {
+  return (
+    <section className="mt-10">
+      {deliveries.map((delivery) => {
+        return <Section delivery={delivery} key={delivery._id} />;
+      })}
+    </section>
+  );
 }
