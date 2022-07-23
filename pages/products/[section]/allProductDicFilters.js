6887ca060a075ac10c2f5508e2d8daa340c16cf1
StR0vErBG <xcreaw1@abv.bg>
2022-07-23 13:22:17 +0200

    chaning to default export

--- pages/products/[section]/allProductDicFilters.js
+++ pages/products/[section]/allProductDicFilters.js
@@ -16,21 +16,23 @@ export const sortByDictionary = [
   //   text: "Цена (низходящ ред)",
   // },
 ];
-export const pageDictionary = [
-  {
-    val: "6",
-    text: "по 6 на страница",
-  },
-  {
-    val: "12",
-    text: "по 12 на страница",
-  },
-  {
-    val: "24",
-    text: "по 24 на страница",
-  },
-  {
-    val: "48",
-    text: "по 48 на страница",
-  },
-];
+// export const pageDictionary = [
+//   {
+//     val: "6",
+//     text: "по 6 на страница",
+//   },
+//   {
+//     val: "12",
+//     text: "по 12 на страница",
+//   },
+//   {
+//     val: "24",
+//     text: "по 24 на страница",
+//   },
+//   {
+//     val: "48",
+//     text: "по 48 на страница",
+//   },
+// ];
+
+export default sortByDictionary;
