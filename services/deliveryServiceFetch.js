86d63ae647966069c6f50e721b2d189edfa5ea2d
StR0vErBG <xcreaw1@abv.bg>
2022-07-23 12:06:00 +0200

    creaint deliveryService FETCH when is component

---
+++ services/deliveryServiceFetch.js
@@ -0,0 +1,15 @@
+export const changeStatus = async (status, deliveryId) => {
+  try {
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status, deliveryId }),
+    };
+    const res = await fetch(`/api/account/deliveries/changeStatus`, options);
+    console.log(res);
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
