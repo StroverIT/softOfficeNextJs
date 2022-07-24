6c97f31943baf787863864236b419b402dd0dcfe
StR0vErBG <xcreaw1@abv.bg>
2022-07-24 19:40:49 +0200

    filtering is working(Almost)

--- pages/products/[section]/index.js
+++ pages/products/[section]/index.js
@@ -74,7 +74,7 @@ export default function Section({ products, sectionName, types }) {
       }
     }
     setArticles(filteredArticles);
-  }, [filters]);
+  }, [filters, products.articles]);
 
   useEffect(() => {
     if (filterMenu) {
