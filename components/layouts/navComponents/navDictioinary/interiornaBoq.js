740f764b6bca72112795f260b3860a25c300fa50
StR0vErBG <xcreaw1@abv.bg>
2022-07-27 23:35:50 +0200

    Adding sections

--- components/layouts/navComponents/navDictioinary/interiornaBoq.js
+++ components/layouts/navComponents/navDictioinary/interiornaBoq.js
@@ -1,2 +1,8 @@
-const interiornaBoq = ["4 Seasons"];
+const interiornaBoq = [
+  "Master",
+  "Master Clean Air",
+  "Master Easy Clean",
+  "Master Eco",
+  "Master Hydrocontrol",
+];
 export default interiornaBoq;
