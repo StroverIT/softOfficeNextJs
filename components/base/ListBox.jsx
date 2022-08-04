75b564fbcec061a0d5e10e25fe4be61e61745527
StR0vErBG <xcreaw1@abv.bg>
2022-08-04 06:02:42 +0200

    fixing styling

--- components/base/ListBox.jsx
+++ components/base/ListBox.jsx
@@ -7,7 +7,7 @@ export default function ListBox({ selected, setSelected, data }) {
   return (
     <Listbox value={selected} onChange={setSelected}>
       <div className="relative mt-1">
-        <Listbox.Button className="relative w-full py-3 pl-3 pr-10 text-lg text-left bg-white border rounded-lg cursor-default border-gray focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+        <Listbox.Button className="relative w-full py-3 pl-3 pr-10  text-left bg-white border rounded-lg cursor-default border-gray focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
           <span className="block truncate">{selected?.name}</span>
           <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
             <HiSelector className="w-5 h-5 text-gray-450" aria-hidden="true" />
