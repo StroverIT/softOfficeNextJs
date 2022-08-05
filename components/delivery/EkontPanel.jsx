3e08424f04e9abf2f6226bcf48a2109c3fade5ff
StR0vErBG <xcreaw1@abv.bg>
2022-08-05 21:39:52 +0200

    There is so many changes but we don't have time..

--- components/delivery/EkontPanel.jsx
+++ components/delivery/EkontPanel.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 // Components
 import Input from "../form/DeliveryInput";
 import TelehponeInput from "../form/TelephoneInput";
 import ListBoxSearch from "../base/ListBoxSearch";
+// Contenxt
+import { InputContext } from "./Context";
+// Constats
+import { EKONT } from "../cart/cartCostants";
+// Inner utils
+import { changeHandler } from "./innerUtils";
 
 export default function MagazinePanel({
   userData,
@@ -11,21 +17,41 @@ export default function MagazinePanel({
   selected,
   setSelected,
 }) {
+  const { inputs, setInputs } = useContext(InputContext);
+
+  useEffect(() => {
+    setInputs((prevState) => ({ ...prevState, typeOfDelivery: EKONT }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    setInputs((prevState) => ({
+      ...prevState,
+      address: {
+        ...prevState.address,
+        office:
+          selected.name.toLowerCase() == "избери офис" ? null : selected.name,
+      },
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected]);
   return (
     <section className="flex flex-col my-10 md:mx-10">
       <section className="items-center justify-center lg:space-x-4 lg:flex">
         <Input
           placeholder="Име и фамилия"
-          id="id and name"
+          id="fullName"
           type="text"
           isReq={false}
+          onChange={(e) => changeHandler(e, setInputs)}
           defValue={userData?.fullName}
         />
         <TelehponeInput
           placeholder="Телефон"
-          id="id and name"
+          id="phoneNumber"
           type="text"
+          getOuterVal={(e) => changeHandler(e, setInputs)}
           isReq={false}
+          onChange={(e) => changeHandler(e, setInputs)}
           phoneNumber={userData.phoneNumber}
         />
       </section>
@@ -33,7 +59,7 @@ export default function MagazinePanel({
         data={officeData}
         selected={selected}
         setSelected={setSelected}
-        inputPlaceholder={"Квартал..."}
+        inputPlaceholder={"Oфис..."}
       />
     </section>
   );
