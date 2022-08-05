ae8d543d80515c9ea0b47f832b837367dbed25ac
StR0vErBG <xcreaw1@abv.bg>
2022-08-05 21:40:04 +0200

    Creating general changeHandler, but sometimes is not working...

---
+++ components/delivery/innerUtils.js
@@ -0,0 +1,9 @@
+export const changeHandler = (e, setInputs) => {
+  const name = e.target.name;
+  const val = e.target.value;
+
+  setInputs((prevState) => ({
+    ...prevState,
+    address: { ...prevState.address, [name]: val },
+  }));
+};
