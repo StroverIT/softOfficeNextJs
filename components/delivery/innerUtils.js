67eca58d234af5e8a59e19b7b0068c626cd17af2
StR0vEr <xcreaw1@abv.bg>
2022-08-17 17:08:55 +0300

    adding space

--- components/delivery/innerUtils.js
+++ components/delivery/innerUtils.js
@@ -1,7 +1,6 @@
 export const changeHandler = (e, setInputs) => {
   const name = e.target.name;
   const val = e.target.value;
-
   setInputs((prevState) => ({
     ...prevState,
     address: { ...prevState.address, [name]: val },
