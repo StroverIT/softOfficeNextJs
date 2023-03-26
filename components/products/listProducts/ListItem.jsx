558a59a86fd9dbd0f53c4de3465e6a27083530c0
StR0vErBG <xcreaw1@abv.bg>
2023-03-26 20:18:43 +0200

    adding loader

--- components/products/listProducts/ListItem.jsx
+++ components/products/listProducts/ListItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 // NextJs
 import Image from "next/image";
@@ -9,6 +9,8 @@ import BuyBtn from "../../base/BuyBtn";
 import OldPrice from "../../priceStyling/OldPrice";
 import Pricing from "../../priceStyling/Pricing";
 import PriceWithQuantity from "../PriceWithQuantity";
+import { GlobalLoadingContext } from "../../layouts/GlobalLoadingContext";
+import { useRouter } from "next/router";
 
 export default function ListItem({
   section,
@@ -18,6 +20,8 @@ export default function ListItem({
   imgUrl,
   route,
 }) {
+  const router = useRouter();
+  const { setLoading } = useContext(GlobalLoadingContext);
   const name = `${section.name != "Обадете се" ? section.name : ""}`;
   const types = item?.tipove?.split(";");
   const [price, setPrice] = useState(null);
@@ -65,81 +69,81 @@ export default function ListItem({
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [price, setPrice, customQtySelected]);
-
+  const changeRouteHandler = () => {
+    router.push(route);
+    setLoading(true);
+  };
   return (
     <div
       className="flex flex-col items-center justify-center w-full break-words transition-shadow bg-white border hover:shadow-2xl border-primary rounded-3xl"
       id={item._id}
     >
       {/* {console.log(route)} */}
-      <Link href={route}>
-        <section className="container cursor-pointer">
-          {articleData?.imgUrl && (
-            <div className="relative w-full h-56 my-10 cursor-pointer">
-              <Image
-                src={imgUrl.includes("https") ? imgUrl : `/uploads/${imgUrl}`}
-                layout="fill"
-                alt={articleData?.imgUrl}
-                className="object-contain"
-              />
-            </div>
-          )}
-          {/* ---- Name --- */}
+      <section
+        className="container cursor-pointer"
+        onClick={changeRouteHandler}
+      >
+        {articleData?.imgUrl && (
+          <div className="relative w-full h-56 my-10 cursor-pointer">
+            <Image
+              src={imgUrl.includes("https") ? imgUrl : `/uploads/${imgUrl}`}
+              layout="fill"
+              alt={articleData?.imgUrl}
+              className="object-contain"
+            />
+          </div>
+        )}
+        {/* ---- Name --- */}
 
-          {/* --- Pricing ----*/}
-          <div className="flex items-center justify-center py-2 border-y border-gray">
-            {price?.forItem &&
-              !price.promoPrice &&
-              section.name != "Обадете се" && (
-                <Pricing
+        {/* --- Pricing ----*/}
+        <div className="flex items-center justify-center py-2 border-y border-gray">
+          {price?.forItem &&
+            !price.promoPrice &&
+            section.name != "Обадете се" && (
+              <Pricing
+                price={parseFloat(price.forItem).toFixed(2).split(".")[0]}
+                priceDec={parseFloat(price.forItem).toFixed(2).split(".")[1]}
+                size="3xl"
+              />
+            )}
+          {price?.promoPrice && section.name != "Обадете се" && (
+            <div className="flex gap-x-5">
+              <div className="text-gray-200">
+                <OldPrice
                   price={parseFloat(price.forItem).toFixed(2).split(".")[0]}
                   priceDec={parseFloat(price.forItem).toFixed(2).split(".")[1]}
                   size="3xl"
-                />
-              )}
-            {price?.promoPrice && section.name != "Обадете се" && (
-              <div className="flex gap-x-5">
-                <div className="text-gray-200">
-                  <OldPrice
-                    price={parseFloat(price.forItem).toFixed(2).split(".")[0]}
-                    priceDec={
-                      parseFloat(price.forItem).toFixed(2).split(".")[1]
-                    }
-                    size="3xl"
-                    NoDDSText={true}
-                  />
-                </div>
-                <Pricing
-                  price={parseFloat(price.promoPrice).toFixed(2).split(".")[0]}
-                  priceDec={
-                    parseFloat(price.promoPrice).toFixed(2).split(".")[1]
-                  }
-                  size="3xl"
+                  NoDDSText={true}
                 />
               </div>
-            )}
-          </div>
-          <h2 className="w-full py-4 text-xl font-semibold text-center ">
-            {name}
-          </h2>
-          {/* ---- Types --- */}
-          <div className="w-full pb-5 pt-5 border-t border-gray ">
-            <ul className="grid grid-cols-2 gap-x-10 ">
-              {types &&
-                types
-                  .filter((e) => e.length < 25)
-                  .splice(0, 5)
-                  .map((type) => {
-                    return (
-                      <li key={type} className="text-sm">
-                        {type}
-                      </li>
-                    );
-                  })}
-            </ul>
-          </div>
-        </section>
-      </Link>
+              <Pricing
+                price={parseFloat(price.promoPrice).toFixed(2).split(".")[0]}
+                priceDec={parseFloat(price.promoPrice).toFixed(2).split(".")[1]}
+                size="3xl"
+              />
+            </div>
+          )}
+        </div>
+        <h2 className="w-full py-4 text-xl font-semibold text-center ">
+          {name}
+        </h2>
+        {/* ---- Types --- */}
+        <div className="w-full pt-5 pb-5 border-t border-gray ">
+          <ul className="grid grid-cols-2 gap-x-10 ">
+            {types &&
+              types
+                .filter((e) => e.length < 25)
+                .splice(0, 5)
+                .map((type) => {
+                  return (
+                    <li key={type} className="text-sm">
+                      {type}
+                    </li>
+                  );
+                })}
+          </ul>
+        </div>
+      </section>
 
       <div className="w-full mt-auto bg-gray-300 rounded-b-3xl">
         {section.name == "Обадете се" && (
