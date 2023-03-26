09d9f850e08c2d3907a3eea6bcce0ed2c968166c
StR0vErBG <xcreaw1@abv.bg>
2023-03-26 14:14:51 +0200

    bittel section

--- pages/bittel/[section].js
+++ pages/bittel/[section].js
@@ -1,19 +1,54 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import ListItem from "../../components/products/listProducts/ListItem";
+import { formatter } from "../../utils/Bittel";
 
-const Bittel = () => {
+import { useDispatch } from "react-redux";
+
+import { addProduct } from "../../utils/helper";
+const Bittel = ({ products }) => {
+  const [articles, setArticles] = useState(null);
+  const dispatch = useDispatch();
   useEffect(() => {
-    (async function () {
-      const data = await fetch(
-        "https://dealers.bittel.bg/bg/api/json/341092012d162fa0d19e2ebad93fc708",
-        {
-          method: "GET",
-        }
-      );
-      const res = await data.json();
-      console.log("Res", res);
-    })();
-  }, []);
-  return <div></div>;
+    setArticles(products);
+  }, [products]);
+  return (
+    <section className="grid gap-10 sm:grid-cols-2 md:grid-cols-3 py-10 container">
+      {articles &&
+        articles.map((item) => {
+          const route = "";
+          return (
+            <ListItem
+              key={item.id}
+              item={item.item}
+              articleData={item.article}
+              route={route}
+              section={item.section}
+              imgUrl={item.article.imgUrl}
+              addProduct={addProduct.bind({}, dispatch)}
+            />
+          );
+        })}
+    </section>
+  );
 };
 
 export default Bittel;
+
+export async function getServerSideProps(context) {
+  const { section } = context.query;
+
+  const res = await fetch(
+    "https://dealers.bittel.bg/bg/api/json/341092012d162fa0d19e2ebad93fc708",
+    {
+      method: "GET",
+    }
+  );
+  const data = await res.json();
+  const products = formatter(data.products, section);
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+      // types: typesObj,
+    },
+  };
+}
