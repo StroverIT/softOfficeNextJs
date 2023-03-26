f02274fb19c01c429285108de9cd9656a097b34a
StR0vErBG <xcreaw1@abv.bg>
2023-03-26 18:47:39 +0200

    changing link for now

--- components/products/listProducts/ListItem.jsx
+++ components/products/listProducts/ListItem.jsx
@@ -72,9 +72,9 @@ export default function ListItem({
       className="flex flex-col items-center justify-center w-full break-words transition-shadow bg-white border hover:shadow-2xl border-primary rounded-3xl"
       id={item._id}
     >
-      <section className="container ">
-        {articleData?.imgUrl && (
-          <Link href={route}>
+      <Link href={route}>
+        <section className="container cursor-pointer">
+          {articleData?.imgUrl && (
             <div className="relative w-full h-56 my-10 cursor-pointer">
               <Image
                 src={imgUrl.includes("https") ? imgUrl : `/uploads/${imgUrl}`}
@@ -83,59 +83,63 @@ export default function ListItem({
                 className="object-contain"
               />
             </div>
-          </Link>
-        )}
-        {/* ---- Name --- */}
+          )}
+          {/* ---- Name --- */}
 
-        {/* --- Pricing ----*/}
-        <div className="flex items-center justify-center py-2 border-y border-gray">
-          {price?.forItem &&
-            !price.promoPrice &&
-            section.name != "Обадете се" && (
-              <Pricing
-                price={parseFloat(price.forItem).toFixed(2).split(".")[0]}
-                priceDec={parseFloat(price.forItem).toFixed(2).split(".")[1]}
-                size="3xl"
-              />
-            )}
-          {price?.promoPrice && section.name != "Обадете се" && (
-            <div className="flex gap-x-5">
-              <div className="text-gray-200">
-                <OldPrice
+          {/* --- Pricing ----*/}
+          <div className="flex items-center justify-center py-2 border-y border-gray">
+            {price?.forItem &&
+              !price.promoPrice &&
+              section.name != "Обадете се" && (
+                <Pricing
                   price={parseFloat(price.forItem).toFixed(2).split(".")[0]}
                   priceDec={parseFloat(price.forItem).toFixed(2).split(".")[1]}
                   size="3xl"
-                  NoDDSText={true}
+                />
+              )}
+            {price?.promoPrice && section.name != "Обадете се" && (
+              <div className="flex gap-x-5">
+                <div className="text-gray-200">
+                  <OldPrice
+                    price={parseFloat(price.forItem).toFixed(2).split(".")[0]}
+                    priceDec={
+                      parseFloat(price.forItem).toFixed(2).split(".")[1]
+                    }
+                    size="3xl"
+                    NoDDSText={true}
+                  />
+                </div>
+                <Pricing
+                  price={parseFloat(price.promoPrice).toFixed(2).split(".")[0]}
+                  priceDec={
+                    parseFloat(price.promoPrice).toFixed(2).split(".")[1]
+                  }
+                  size="3xl"
                 />
               </div>
-              <Pricing
-                price={parseFloat(price.promoPrice).toFixed(2).split(".")[0]}
-                priceDec={parseFloat(price.promoPrice).toFixed(2).split(".")[1]}
-                size="3xl"
-              />
-            </div>
-          )}
-        </div>
-        <h2 className="w-full py-4 text-xl font-semibold text-center ">
-          {name}
-        </h2>
-        {/* ---- Types --- */}
-        <div className="w-full pb-5 pt-5 border-t border-gray ">
-          <ul className="grid grid-cols-2 gap-x-10 ">
-            {types &&
-              types
-                .filter((e) => e.length < 25)
-                .splice(0, 5)
-                .map((type) => {
-                  return (
-                    <li key={type} className="text-sm">
-                      {type}
-                    </li>
-                  );
-                })}
-          </ul>
-        </div>
-      </section>
+            )}
+          </div>
+          <h2 className="w-full py-4 text-xl font-semibold text-center ">
+            {name}
+          </h2>
+          {/* ---- Types --- */}
+          <div className="w-full pb-5 pt-5 border-t border-gray ">
+            <ul className="grid grid-cols-2 gap-x-10 ">
+              {types &&
+                types
+                  .filter((e) => e.length < 25)
+                  .splice(0, 5)
+                  .map((type) => {
+                    return (
+                      <li key={type} className="text-sm">
+                        {type}
+                      </li>
+                    );
+                  })}
+            </ul>
+          </div>
+        </section>
+      </Link>
 
       <div className="w-full mt-auto bg-gray-300 rounded-b-3xl">
         {section.name == "Обадете се" && (
