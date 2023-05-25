aac209389cedf9115c4656292ff35be6e3e5b67a
StR0vErBG <xcreaw1@abv.bg>
2023-05-25 18:15:03 +0200

    fixing links

--- components/Home/Carousel.js
+++ components/Home/Carousel.js
@@ -8,39 +8,39 @@ const swiperPag = [
       src: "/carousel/!Image Volume.png",
   
       key: "image volume ",
-      pageUrl: "/products/hartiq/6374d3379f74a9b4f4eb61a2",
+      pageUrl: "products/hartiq/6374d3379f74a9b4f4eb61a2?itemId=6374d3379f74a9b4f4eb61a4",
     },
     {
       src: "/carousel/!Kasova kniga.png",
   
       key: "Kasova kniga",
       pageUrl:
-        "/products/formylqri/621779090125b0cc2f1484a1#621779090125b0cc2f1484a9",
+        "/products/formylqri/621779090125b0cc2f1484a1?itemId=621779090125b0cc2f1484a9",
     },
     {
       src: "/carousel/!Papka klasior.png",
   
       key: "Papka klasior",
-      pageUrl: "/products/klasiori/62015d6437d09b4aa956e968",
+      pageUrl: "/products/klasiori/62015d6437d09b4aa956e968?itemId=62015d6437d09b4aa956e969",
     },
     {
       src: "/carousel/hero_slides_3.png",
   
       key: "Тонер Касета",
-      pageUrl: "/products/skeneri/61f4128f031938ea821770f3",
+      pageUrl: "/products/skeneri",
     },
   
     {
       src: "/carousel/ferero.png",
   
       key: "test carousel for my monday version 6",
-      pageUrl: "/products/product",
+      pageUrl: "/",
     },
     {
       src: "/carousel/freedeliveryLego.png",
   
       key: "test carousel for my monday version 7",
-      pageUrl: "/products/product",
+      pageUrl: "/",
     },
   ];
 
